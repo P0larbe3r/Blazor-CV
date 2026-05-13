db734e571710a4cd6b95aff6fad50f8cfd49b105
Josh <jjw.watson2710@gmail.com>
2026-05-13 19:50:33 +0100

    Repo init

--- obj/Debug/net10.0/Blazor CV App.AssemblyInfo.cs
+++ obj/Debug/net10.0/Blazor CV App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor_CV_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95574e918c8bf2aba18c76b202ffc804905a19d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor_CV_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor_CV_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
